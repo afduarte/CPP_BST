15ed75dff7c8fe5ee53a7a8ee4f4c27ccf6f9eb5
Antero Duarte <a.fduarte1@gmail.com>
2016-03-15 12:41:31 +0000

    Implemented basic constructor

--- src/binary_tree.cpp
+++ src/binary_tree.cpp
@@ -1,73 +1,80 @@
 #include "binary_tree.h"
 
 // Creates an empty binary tree
-binary_tree::binary_tree()
-{
+binary_tree::binary_tree(){
+    this->tree = new node;
 }
 
 // Creates a binary tree with an initial value to store
-binary_tree::binary_tree(int value)
-{
+binary_tree::binary_tree(int value){
+    this->tree = new node;
+    this->tree->data=value;
 }
 
 // Creates a binary tree from a collection of existing values
-binary_tree::binary_tree(const std::vector<int> &values)
-{
+binary_tree::binary_tree(const std::vector<int> &values){
 
 }
 
 // Creates a binary tree by copying an existing tree
-binary_tree::binary_tree(const binary_tree &rhs)
-{
+binary_tree::binary_tree(const binary_tree &rhs){
 
 }
 
 // Destroys (cleans up) the tree
-binary_tree::~binary_tree()
-{
+binary_tree::~binary_tree(){
     
 }
 
 
 // Adds a value to the tree
-void binary_tree::insert(int value)
-{
+void binary_tree::insert(int value){
 }
 
 // Removes a value from the tree
-void binary_tree::remove(int value)
-{
+void binary_tree::remove(int value){
 
 }
 
 // Checks if a value is in the tree
-bool binary_tree::exists(int value) const
-{
+bool binary_tree::exists(int value) const{
     return true;
 }
 
 // Prints the tree to standard out in numerical order
-std::string binary_tree::inorder() const
-{
-    return std::string("");
+std::string binary_tree::inorder() const{
+    if(this->tree != nullptr){
+        std::string text = "";
+        if(this->tree->left == nullptr){
+            return text;
+        }else{
+            this->tree->left->inorder();
+        }
+        
+        if(text != "") text+= " ";
+        
+        text += std::string(this->data);
+        
+        // text += this->tree->right->inorder();
+        return text;
+    }else{
+        return std::string("");
+    }
 }
 
 // Prints the tree in pre-order
-std::string binary_tree::preorder() const
-{
+std::string binary_tree::preorder() const{
     return std::string("");
 }
 
 // Prints the tree in post-order
-std::string binary_tree::postorder() const
-{
+std::string binary_tree::postorder() const{
     return std::string("");
 }
 
 // Copy assignment operator
-binary_tree& binary_tree::operator=(const binary_tree &other)
-{
-    return binary_tree();
+binary_tree& binary_tree::operator=(const binary_tree &other){
+    // return binary_tree();
 }
 
 // Checks if two trees are equal
@@ -83,25 +90,21 @@ bool binary_tree::operator!=(const binary_tree &other) const
 }
 
 // Inserts a new value into the binary tree
-binary_tree& binary_tree::operator+(int value)
-{
-    return binary_tree();
+binary_tree& binary_tree::operator+(int value){
+    // return binary_tree();
 }
 
 // Removes a value from the binary tree
-binary_tree& binary_tree::operator-(int value)
-{
-    return binary_tree();
+binary_tree& binary_tree::operator-(int value){
+    // return binary_tree();
 }
 
 // Reads in values from an input stream into the tree
-std::istream& operator>>(std::istream &in, binary_tree &value)
-{
+std::istream& operator>>(std::istream &in, binary_tree &value){
     return in;
 }
 
 // Writes the values, in-order, to an output stream
-std::ostream& operator<<(std::ostream &out, const binary_tree &value)
-{
+std::ostream& operator<<(std::ostream &out, const binary_tree &value){
     return out;
 }
